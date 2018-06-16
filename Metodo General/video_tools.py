1c135d0c6014aaee0b8b7b21e9ad054e9a74a182
evd995 <egvillalobos@uc.cl>
2018-06-16 10:18:04 -0400

    Se agrega implementacion de metodo general

---
+++ Metodo General/video_tools.py
@@ -0,0 +1,54 @@
+import cv2
+import numpy as np
+
+def load_video(path):
+    video = cv2.VideoCapture(path)
+
+    M = None # Video como matriz
+
+    try: 
+        while video.isOpened():
+            ret, frame = video.read()
+            
+            if ret:
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                if M is None:
+                    M = frame.flatten()
+                else:
+                    M = np.column_stack((M, frame.flatten()))
+            else:
+                break
+    finally:
+        video.release()
+    
+    return M
+
+def save_demo_video(path, D, L, S):
+    """
+    Funcion pensada para guardar especificamente el video del
+    demo.
+    
+    Primera fila, D
+    Segunda fila, S
+    Tercera fila, L
+    """
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter(path, fourcc, 20.0, (100,300))
+
+
+    try:
+        print('Creating Video')
+        for i in range(L.shape[1]):
+            D_frame = D[:, i].reshape(100,100).astype(np.uint8)
+            D_frame = cv2.cvtColor(D_frame, cv2.COLOR_GRAY2BGR)
+            
+            L_frame = L[:, i].reshape(100,100).astype(np.uint8)
+            L_frame = cv2.cvtColor(L_frame, cv2.COLOR_GRAY2BGR)
+
+            S_frame = S[:, i].reshape(100,100).astype(np.uint8)
+            S_frame = cv2.cvtColor(S_frame, cv2.COLOR_GRAY2BGR)
+
+            out.write(cv2.vconcat([D_frame, L_frame, S_frame]))
+    finally:
+        print('Releasing Video')
+        out.release()
