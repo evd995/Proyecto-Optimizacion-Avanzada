bc47530d3690085c520963fec49426150a4b5c4c
evd995 <egvillalobos@uc.cl>
2018-06-18 01:12:08 -0400

    video_tools ahora tiene save_video general y load_video retorna tamaño del video

--- Metodo General/video_tools.py
+++ Metodo General/video_tools.py
@@ -3,7 +3,8 @@ import numpy as np
 
 def load_video(path):
     video = cv2.VideoCapture(path)
-
+    
+    size = None
     M = None # Video como matriz
 
     try: 
@@ -12,6 +13,8 @@ def load_video(path):
             
             if ret:
                 frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                size = frame.shape
+                
                 if M is None:
                     M = frame.flatten()
                 else:
@@ -21,7 +24,7 @@ def load_video(path):
     finally:
         video.release()
     
-    return M
+    return M, size
 
 def save_demo_video(path, D, L, S):
     """
@@ -34,6 +37,36 @@ def save_demo_video(path, D, L, S):
     """
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
     out = cv2.VideoWriter(path, fourcc, 20.0, (100,300))
+
+
+    try:
+        print('Creating Video')
+        for i in range(L.shape[1]):
+            D_frame = D[:, i].reshape(100,100).astype(np.uint8)
+            D_frame = cv2.cvtColor(D_frame, cv2.COLOR_GRAY2BGR)
+            
+            L_frame = L[:, i].reshape(100,100).astype(np.uint8)
+            L_frame = cv2.cvtColor(L_frame, cv2.COLOR_GRAY2BGR)
+
+            S_frame = S[:, i].reshape(100,100).astype(np.uint8)
+            S_frame = cv2.cvtColor(S_frame, cv2.COLOR_GRAY2BGR)
+
+            out.write(cv2.vconcat([D_frame, L_frame, S_frame]))
+    finally:
+        print('Releasing Video')
+        out.release()
+        
+def save_video(path, video_size, D, L, S):
+    """
+    Funcion pensada para guardar especificamente el video del
+    demo.
+    
+    Primera fila, D
+    Segunda fila, S
+    Tercera fila, L
+    """
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter(path, fourcc, 20.0, (video_size[1],3*video_size[0]))
 
 
     try:
