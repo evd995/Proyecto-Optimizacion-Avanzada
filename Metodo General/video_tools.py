ef7c8e1ec0b4488750ce79af6169d85357c82366
evd995 <egvillalobos@uc.cl>
2018-06-18 02:26:44 -0400

    Test con mini_trafico.avi

--- Metodo General/video_tools.py
+++ Metodo General/video_tools.py
@@ -72,13 +72,13 @@ def save_video(path, video_size, D, L, S):
     try:
         print('Creating Video')
         for i in range(L.shape[1]):
-            D_frame = D[:, i].reshape(100,100).astype(np.uint8)
+            D_frame = D[:, i].reshape(video_size).astype(np.uint8)
             D_frame = cv2.cvtColor(D_frame, cv2.COLOR_GRAY2BGR)
             
-            L_frame = L[:, i].reshape(100,100).astype(np.uint8)
+            L_frame = L[:, i].reshape(video_size).astype(np.uint8)
             L_frame = cv2.cvtColor(L_frame, cv2.COLOR_GRAY2BGR)
 
-            S_frame = S[:, i].reshape(100,100).astype(np.uint8)
+            S_frame = S[:, i].reshape(video_size).astype(np.uint8)
             S_frame = cv2.cvtColor(S_frame, cv2.COLOR_GRAY2BGR)
 
             out.write(cv2.vconcat([D_frame, L_frame, S_frame]))
